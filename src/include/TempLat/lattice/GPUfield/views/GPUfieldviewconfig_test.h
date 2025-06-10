03784f8e2327365144a4a10af4e989a11b940816
Franz R. Sattler <fsattler@physik.uni-bielefeld.de>
2025-06-10 16:19:00 +0200

    Try to redesign the field class...

---
+++ src/include/TempLat/lattice/GPUfield/views/GPUfieldviewconfig_test.h
@@ -0,0 +1,18 @@
+#ifndef TEMPLAT_LATTICE_GPUFIELD_VIEWS_GPUFIELDVIEWCONFIG_TEST_H
+#define TEMPLAT_LATTICE_GPUFIELD_VIEWS_GPUFIELDVIEWCONFIG_TEST_H
+#include "GPUfieldviewconfig.h"
+
+/* This file is part of CosmoLattice, available at www.cosmolattice.net .
+   Copyright Daniel G. Figueroa, Adrien Florio, Francisco Torrenti and Wessel Valkenburg.
+   Released under the MIT license, see LICENSE.md. */
+
+// File info: Main contributor(s): Franz R. Sattler,  Year: 2025
+
+template<typename T>
+inline void TempLat::GPUConfigView<T>::Test(TempLat::TDDAssertion &tdd) {
+    LatticeParameters<double> latp;
+    GPUConfigView<double> config_view("view", latp);
+    tdd.verify(true);
+}
+
+#endif
