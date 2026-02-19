404ecd6ef2538f70a2a2aff4b72fb2ba2677a85c
Franz R. Sattler <fsattler@physik.uni-bielefeld.de>
2026-02-19 18:07:23 +0100

    fixed the kokkos memory test: forgot to take care the all layouts were from the right.

--- src/include/TempLat/parallel/devices/kokkos/kokkos_memory_test.h
+++ src/include/TempLat/parallel/devices/kokkos/kokkos_memory_test.h
@@ -12,7 +12,7 @@ void TempLat::KokkosMemoryTester::Test(TempLat::TDDAssertion &tdd)
   // Simplest test
   {
     say << "Testing 1D copy device to host";
-    Kokkos::View<double *, Kokkos::DefaultExecutionSpace> a("a", 10);
+    Kokkos::View<double *, Kokkos::LayoutRight, Kokkos::DefaultExecutionSpace> a("a", 10);
     Kokkos::parallel_for(
         Kokkos::RangePolicy<Kokkos::DefaultExecutionSpace>((size_t)0, 10), DEVICE_LAMBDA(size_t i) { a(i) = i; });
     std::vector<double> host_a(10);
@@ -31,7 +31,7 @@ void TempLat::KokkosMemoryTester::Test(TempLat::TDDAssertion &tdd)
   // Test with subview
   {
     say << "Testing 1D copy device to host with subview";
-    Kokkos::View<double *, Kokkos::DefaultExecutionSpace> _a("a", 10);
+    Kokkos::View<double *, Kokkos::LayoutRight, Kokkos::DefaultExecutionSpace> _a("a", 10);
 
     auto a = Kokkos::subview(_a, std::make_pair((size_t)2, (size_t)8));
 
@@ -55,10 +55,10 @@ void TempLat::KokkosMemoryTester::Test(TempLat::TDDAssertion &tdd)
   {
     say << "Testing 2D copy device to host";
 
-    Kokkos::View<double **, Kokkos::DefaultExecutionSpace> a("a", 10, 10);
+    Kokkos::View<double **, Kokkos::LayoutRight, Kokkos::DefaultExecutionSpace> a("a", 10, 10);
     Kokkos::parallel_for(
         Kokkos::MDRangePolicy<Kokkos::DefaultExecutionSpace, Kokkos::Rank<2>>({0, 0}, {10, 10}),
-        DEVICE_LAMBDA(size_t i, size_t j) { a(i, j) = j * 10 + i; });
+        DEVICE_LAMBDA(size_t i, size_t j) { a(i, j) = i * 10 + j; });
 
     std::vector<double> host_a(100);
 
@@ -79,12 +79,12 @@ void TempLat::KokkosMemoryTester::Test(TempLat::TDDAssertion &tdd)
   {
     say << "Testing 2D copy device to host with subview";
 
-    Kokkos::View<double **, Kokkos::DefaultExecutionSpace> _a("a", 10, 10);
+    Kokkos::View<double **, Kokkos::LayoutRight, Kokkos::DefaultExecutionSpace> _a("a", 10, 10);
     Kokkos::parallel_for(
         Kokkos::MDRangePolicy<Kokkos::DefaultExecutionSpace, Kokkos::Rank<2>>({0, 0}, {10, 10}),
-        DEVICE_LAMBDA(size_t i, size_t j) { _a(i, j) = j * 10 + i; });
+        DEVICE_LAMBDA(size_t i, size_t j) { _a(i, j) = i * 10 + j; });
 
-    auto a = Kokkos::subview(_a, std::make_pair((size_t)2, (size_t)8), std::make_pair((size_t)1, (size_t)9));
+    auto a = Kokkos::subview(_a, std::make_pair((size_t)1, (size_t)9), std::make_pair((size_t)2, (size_t)8));
 
     std::vector<double> host_a(6 * 8);
 
@@ -105,12 +105,12 @@ void TempLat::KokkosMemoryTester::Test(TempLat::TDDAssertion &tdd)
   {
     say << "Testing 2D copy device to host with subviews (large arrays)";
 
-    Kokkos::View<double **, Kokkos::DefaultExecutionSpace> _a("a", 128, 128);
+    Kokkos::View<double **, Kokkos::LayoutRight, Kokkos::DefaultExecutionSpace> _a("a", 128, 128);
     Kokkos::parallel_for(
         Kokkos::MDRangePolicy<Kokkos::DefaultExecutionSpace, Kokkos::Rank<2>>({0, 0}, {128, 128}),
-        DEVICE_LAMBDA(size_t i, size_t j) { _a(i, j) = j * 10 + i; });
+        DEVICE_LAMBDA(size_t i, size_t j) { _a(i, j) = i * 10 + j; });
 
-    auto a = Kokkos::subview(_a, std::make_pair((size_t)2, (size_t)120), std::make_pair((size_t)4, (size_t)110));
+    auto a = Kokkos::subview(_a, std::make_pair((size_t)4, (size_t)110), std::make_pair((size_t)2, (size_t)120));
 
     std::vector<double> host_a(118 * 106);
 
