3efcbd4e22f02462d2474e38b5002bda30b2068b
Franz R. Sattler <fsattler@physik.uni-bielefeld.de>
2026-03-30 18:27:31 +0200

    make sure couplings are set in the couplingsmanager

--- include/CosmoInterface/couplingsmanager.h
+++ include/CosmoInterface/couplingsmanager.h
@@ -78,7 +78,8 @@ namespace TempLat
                                                         " couplings were specified while " + std::to_string(NGauge) +
                                                         " gauge fields are present. Abort."));
       // Set the gauge field couplings g and the effective charges g*Q for each matter-gauge coupling.
-      gs = couplings;
+      for (size_t i = 0; i < NGauge && i < couplings.size(); ++i)
+        gs[i] = couplings[i];
       if constexpr (NGauge > 0) {
         size_t count = 0;
         for (size_t i = 0; i < doesCouples.size(); ++i) {
