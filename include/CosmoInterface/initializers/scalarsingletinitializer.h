dec985e44c433d676ba0e8390b48e1616a756910
nloayzar <nicolr_1609@hotmail.com>
2026-05-20 16:35:09 +0200

    Restore scalarsingletinitializer to previous status before inclusion of ICs for scalar if coupled to U1 since it broke ICs for other cases.

--- include/CosmoInterface/initializers/scalarsingletinitializer.h
+++ include/CosmoInterface/initializers/scalarsingletinitializer.h
@@ -48,15 +48,7 @@ namespace TempLat
               auto& s = model.extPS[i];
              
               if (s == Constants::defaultString || s.empty() || s == "None" || s == "none") {
-                ForLoop(k, 0, Model::NU1-1,
-                    if(Model::ScalarU1AxionCouplings::couples(i,k))
-                    {
-                      model.fldS(i) = 0.0;                  // If a Scalar field is coupled to an AxionU1 field it is intialized with zero fluctuations.
-                      model.piS(i) = 0.0 ;
-                    }
-                    else{
                       fg.conjugateGaussianFluctuations(model, model.fldS(i), model.piS(i), model.masses2S[i], model.aDotI, kCutOff);                 
-                    });
               } else {
                 extps.conjugateGaussianInputFluctuations(model, model.fldS(i), model.piS(i), s, kCutOff, PSType);
               }
